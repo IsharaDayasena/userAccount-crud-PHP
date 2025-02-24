7685d70990671d015875e48b5d1ec0afcd258add
IsharaDayasena <141900612+IsharaDayasena@users.noreply.github.com>
2025-02-24 18:37:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Account</title>
+    <link rel="stylesheet" href="./css/insert.css">
+</head>
+<body>
+
+        <h1>Insert User Account</h1>
+        <table>
+            <thead>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+
+</thead>
+        </table>
+
+    
+</body>
+</html>
